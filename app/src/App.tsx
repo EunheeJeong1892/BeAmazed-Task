3d110ee01c66ca4ee4e082866f0a42c5926aec68
EunheeJeong1892 <echoeunhee@gmail.com>
2024-12-06 15:13:10 +0000

    init: project initiate

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import IntroGenerator from "./components/IntroGenerator";
+
+function App() {
+  return (
+    <>
+      <IntroGenerator />
+    </>
+  );
+}
+
+export default App;
---
+++ app/src/components/IntroGenerator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IntroGenerator: React.FC = () => {
+  return <div>Intro generator</div>;
+};
+
+export default IntroGenerator;
